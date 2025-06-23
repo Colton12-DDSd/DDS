930e2640b65d6aa14a6962cac08aa1aa9a734ddd
Colton12-DDSd <colton12.cb@gmail.com>
2025-06-22 21:13:06 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import './globals.css' // or '../styles/globals.css' if it's in a styles folder
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
