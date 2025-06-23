6b1588f1713e483d7f10d8071fc71e1016e9647a
Colton12-DDSd <colton12.cb@gmail.com>
2025-06-22 20:57:34 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import 'globals.css'
+import './globals.css'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-gray-900 text-gray-100">
+      <body>
         {children}
       </body>
     </html>
